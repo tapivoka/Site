4897317008c7309051d4558044ff024317e1a2da
Nikolay Dozmorov <ndozmorov@gmail.com>
2019-09-13 17:21:20 +0700

    Add spinner for contacts map

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
 import { Map, Placemark, YMaps } from "react-yandex-maps"
 
@@ -8,6 +8,10 @@ import { faAt, faMapMarkerAlt, faMobileAlt, faUniversity } from "@fortawesome/fr
 import "./contacts.scss"
 
 export default ({ data }) => {
+  const [mapLoaded, setMapLoaded] = useState(false)
+
+  const handleMapLoaded = () => setMapLoaded(true)
+
   return (
     <Layout>
       <div>
@@ -18,7 +22,8 @@ export default ({ data }) => {
         <div className="contacts">
           <div className="contacts__info card card--left">
             <FontAwesomeIcon className="contacts__icon" icon={faUniversity} />
-            <div className="contacts__text">Институт химической кинетики и горения им.&nbsp;В.В.&nbsp;Воеводского Сибирского отделения <br/>Российской академии наук</div>
+            <div className="contacts__text">Институт химической кинетики и горения им.&nbsp;В.В.&nbsp;Воеводского Сибирского отделения <br />Российской академии наук
+            </div>
 
             <FontAwesomeIcon className="contacts__icon" icon={faMapMarkerAlt} />
             <div className="contacts__text">630090 <br /> г. Новосибирск <br /> ул. Институтская, 3</div>
@@ -30,8 +35,14 @@ export default ({ data }) => {
             <div className="contacts__text">baklanov@kinetics.nsc.ru</div>
           </div>
           <div className="contacts__map card">
+            {
+              !mapLoaded
+              &&
+              <div className="loader" />
+            }
             <YMaps>
               <Map
+                onLoad={handleMapLoaded}
                 defaultState={{
                   center: [54.844909, 83.11512],
                   zoom: 16,
