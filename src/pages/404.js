c2a24092163abae0141999c5210d62e37b6b7fde
Nikolay Dozmorov <ndozmorov@gmail.com>
2019-09-13 19:38:38 +0700

    Add custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React  from "react"
+import Layout from "../components/layout"
+
+export default () => {
+  return (
+    <Layout>
+      <div>
+        <h1>
+          Страница не найдена
+        </h1>
+      </div>
+    </Layout>
+  )
+}
