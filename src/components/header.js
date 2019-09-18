e0548dd3f166053bdcd2eba2c6a0f7230bf66995
Nikolay Dozmorov <ndozmorov@gmail.com>
2019-09-18 12:42:34 +0300

    Added menu for mobile version

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,33 @@
-import React from "react"
-import { Link } from "../intl/link"
+import React, { useState } from "react"
+import cn from "classnames"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons"
 
 import { Nav } from "./nav"
+import { Link } from "../intl/link"
+
 
 import "./header.scss"
 
 export const Header = () => {
+  const [isNavVisible, setNavVisibility] = useState(false)
+
+  const toogleNavVisibility = () => {
+    setNavVisibility(!isNavVisible)
+  }
+
   return (
     <div className="header">
       <div className="header__container">
         <div className="header__title">
           <Link className="header__title-link" to="/">mpd_group</Link>
         </div>
-        <div className="header__nav">
+        <div className="header__menu-icon" onClick={toogleNavVisibility}>
+          <FontAwesomeIcon icon={isNavVisible ? faTimes : faBars} />
+
+
+        </div>
+        <div className={cn("header__nav", { "header__nav--visible": isNavVisible })}>
           <Nav />
         </div>
       </div>
